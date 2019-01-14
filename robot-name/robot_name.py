f1d0574e9ab3dd1d77ad52c9cc7082418120954e
Nick Petzold <nickpetzold@gmail.com>
2019-01-14 22:27:37 +0000

    Started robot name

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,3 +1,12 @@
+import weakref
+
 class Robot(object):
+    instances = []
     def __init__(self):
-        pass
+        self.__class__.instances.append(weakref.proxy(self))
+
+        self.name = name
+
+    def generate_name:
+        prefix = random.sample(list(range(A-Z)), k = 2)
+        suffix =
