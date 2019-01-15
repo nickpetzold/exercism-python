b8d3c228a51e8ce21a1e1dd0ff889af130a8aaa2
Nick Petzold <nickpetzold@gmail.com>
2019-01-15 20:39:11 +0000

    Robot name completed

--- robot-name/robot_name.py
+++ robot-name/robot_name.py
@@ -1,12 +1,24 @@
-import weakref
+import random
 
 class Robot(object):
     instances = []
     def __init__(self):
-        self.__class__.instances.append(weakref.proxy(self))
+        self.set_name()
+        Robot.instances.append(self.name)
 
+    def generate_name(self):
+        name = ''
+        for _ in range(2):
+            name += random.sample('ABCDEFGHIJKLMNOPQRSTUVWXYZ', 1)[0]
+        for _ in range(3):
+            name += random.sample('0123456789', 1)[0]
+        return name
+
+    def reset(self):
+        self.set_name()
+
+    def set_name(self):
+        name = self.generate_name()
+        while name in Robot.instances:
+            name = self.generate_name()
         self.name = name
-
-    def generate_name:
-        prefix = random.sample(list(range(A-Z)), k = 2)
-        suffix =
